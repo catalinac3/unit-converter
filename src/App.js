2d1cfe37d9cae6898731c11af3d448e169a37e2c
catalina <cdianacatalina@gmail.com>
2021-04-30 22:39:44 +0200

    input changing dropdown works

--- src/App.js
+++ src/App.js
@@ -11,43 +11,72 @@ class App extends Component {
       { name: "inches", number: "0" },
       { name: "feet", number: "0" },
     ],
-    unitSelected: {value: 'm'}
+    unitSelected: { value: "m" },
+    inputField: { value: "0" },
   };
- 
+
+  conversionFactors = (unit) => {
+    switch (unit) {
+      case "m":
+        return [100, 1, 39.37, 3.28];
+      case "cm":
+        return [1, 0.01, 0.39, 0.033];
+      case "in":
+        return [2.54, 0.0254, 1, 0.083];
+      case "ft":
+        return [30.48, 0.305, 12, 1];
+    }
+  };
 
   /*trigger by the change of the input field */
   unitConversion = (event) => {
-    // sets the conversion parameters depending on the drop box selected unit 
-    let setUnit;
-    switch (this.state.unitSelected.value) {
-      case "m":
-        setUnit = [100, 1, 39.37,3.28];
-        break
-      case "cm":
-        setUnit = [1, 0.01, 0.39, 0.033];
-        break
-      case "in":
-        setUnit = [2.54, 0.0254, 1, 0.083];
-        break
-      case "ft":
-        setUnit = [30.48, 0.305, 12, 1];
-        break
-    }
- 
+    const setUnit = this.conversionFactors(this.state.unitSelected.value);
+
     this.setState({
       units: [
-        { name: "centimeters", number: event.target.value * setUnit[0] },
-        { name: "meters", number: event.target.value * setUnit[1] },
-        { name: "inches", number: event.target.value * setUnit[2] },
-        { name: "feet", number: event.target.value * setUnit[3] },
+        {
+          name: "centimeters",
+          number: (event.target.value * setUnit[0]).toFixed(1),
+        },
+        {
+          name: "meters",
+          number: (event.target.value * setUnit[1]).toFixed(1),
+        },
+        {
+          name: "inches",
+          number: (event.target.value * setUnit[2]).toFixed(1),
+        },
+        { name: "feet", number: (event.target.value * setUnit[3]).toFixed(1) },
       ],
+      inputField: { value: event.target.value },
     });
   };
-  
+
   // trigger by change on the dropbox
-  unitSelected= (event) => {
-    this.setState({unitSelected: {value: event.target.value}}, () =>{console.log (this.state)})
-  }
+  unitSelected = (event) => {
+    const setUnit = this.conversionFactors(event.target.value);
+    this.setState({
+      units: [
+        {
+          name: "centimeters",
+          number: this.state.inputField.value * setUnit[0],
+        },
+        {
+          name: "meters",
+          number: (this.state.inputField.value * setUnit[1]).toFixed(1),
+        },
+        {
+          name: "inches",
+          number: (this.state.inputField.value * setUnit[2]).toFixed(1),
+        },
+        {
+          name: "feet",
+          number: (this.state.inputField.value * setUnit[3]).toFixed(1),
+        },
+      ],
+      unitSelected: { value: event.target.value },
+    });
+  };
 
   render() {
     return (
